507a2e2b44c42570727eca13872458e8f94cccd5
Van Tien <tientin995@gmail.com>
2016-06-16 21:18:12 +0700

    no message

--- DataAccessLayer/PresentationLayer/M_DangNhap.Designer.cs
+++ DataAccessLayer/PresentationLayer/M_DangNhap.Designer.cs
@@ -28,46 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtusername = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.buttonDangNhap = new System.Windows.Forms.Button();
             this.txtpass = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.buttonDangNhap = new System.Windows.Forms.Button();
-            this.buttonThoat = new System.Windows.Forms.Button();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // groupBox1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(103, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tên đăng nhập";
+            this.groupBox1.BackgroundImage = global::PresentationLayer.Properties.Resources.vangbac_nhancuoi;
+            this.groupBox1.Controls.Add(this.buttonThoat);
+            this.groupBox1.Controls.Add(this.buttonDangNhap);
+            this.groupBox1.Controls.Add(this.txtpass);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtusername);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 305);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
             // 
-            // txtusername
+            // buttonThoat
             // 
-            this.txtusername.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.buttonThoat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtusername.Location = new System.Drawing.Point(210, 78);
-            this.txtusername.Name = "txtusername";
-            this.txtusername.Size = new System.Drawing.Size(178, 20);
-            this.txtusername.TabIndex = 1;
-            this.txtusername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtusername_KeyPress);
+            this.buttonThoat.Location = new System.Drawing.Point(233, 183);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(71, 24);
+            this.buttonThoat.TabIndex = 11;
+            this.buttonThoat.Text = "Thoát";
+            this.buttonThoat.UseVisualStyleBackColor = true;
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // buttonDangNhap
+            // 
+            this.buttonDangNhap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDangNhap.Location = new System.Drawing.Point(84, 183);
+            this.buttonDangNhap.Name = "buttonDangNhap";
+            this.buttonDangNhap.Size = new System.Drawing.Size(71, 24);
+            this.buttonDangNhap.TabIndex = 10;
+            this.buttonDangNhap.Text = "Đăng nhập";
+            this.buttonDangNhap.UseVisualStyleBackColor = true;
+            this.buttonDangNhap.Click += new System.EventHandler(this.buttonDangNhap_Click);
             // 
             // txtpass
             // 
             this.txtpass.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtpass.Location = new System.Drawing.Point(210, 118);
+            this.txtpass.Location = new System.Drawing.Point(158, 128);
             this.txtpass.Name = "txtpass";
-            this.txtpass.Size = new System.Drawing.Size(178, 20);
-            this.txtpass.TabIndex = 3;
+            this.txtpass.Size = new System.Drawing.Size(174, 20);
+            this.txtpass.TabIndex = 9;
             this.txtpass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtpass_KeyPress);
             // 
             // label2
@@ -76,64 +99,59 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(103, 121);
+            this.label2.Location = new System.Drawing.Point(51, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
-            this.label2.TabIndex = 2;
+            this.label2.TabIndex = 8;
             this.label2.Text = "Mật khẩu";
             // 
-            // buttonDangNhap
+            // txtusername
             // 
-            this.buttonDangNhap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.txtusername.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonDangNhap.Location = new System.Drawing.Point(146, 223);
-            this.buttonDangNhap.Name = "buttonDangNhap";
-            this.buttonDangNhap.Size = new System.Drawing.Size(75, 23);
-            this.buttonDangNhap.TabIndex = 4;
-            this.buttonDangNhap.Text = "Đăng nhập";
-            this.buttonDangNhap.UseVisualStyleBackColor = true;
-            this.buttonDangNhap.Click += new System.EventHandler(this.buttonDangNhap_Click);
+            this.txtusername.Location = new System.Drawing.Point(158, 88);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(174, 20);
+            this.txtusername.TabIndex = 7;
+            this.txtusername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtusername_KeyPress);
             // 
-            // buttonThoat
+            // label1
             // 
-            this.buttonThoat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonThoat.Location = new System.Drawing.Point(295, 223);
-            this.buttonThoat.Name = "buttonThoat";
-            this.buttonThoat.Size = new System.Drawing.Size(75, 23);
-            this.buttonThoat.TabIndex = 5;
-            this.buttonThoat.Text = "Thoát";
-            this.buttonThoat.UseVisualStyleBackColor = true;
-            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tên đăng nhập";
             // 
             // M_DangNhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 318);
-            this.Controls.Add(this.buttonThoat);
-            this.Controls.Add(this.buttonDangNhap);
-            this.Controls.Add(this.txtpass);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtusername);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(404, 305);
+            this.Controls.Add(this.groupBox1);
             this.Name = "M_DangNhap";
             this.Text = "Chào mừng đến với phần mềm quản lý vàng bạc đá quý 3TM";
             this.Load += new System.EventHandler(this.M_DangNhap_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonThoat;
+        private System.Windows.Forms.Button buttonDangNhap;
         private System.Windows.Forms.TextBox txtpass;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button buttonDangNhap;
-        private System.Windows.Forms.Button buttonThoat;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Label label1;
+
     }
 }
